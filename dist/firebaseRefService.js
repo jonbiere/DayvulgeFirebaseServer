c884c99f584d61457faa3f764e64d4432d0ec4c7
Jon Biere <jon.biere@gmail.com>
2017-03-18 00:27:13 -0500

    using .ebignore instead of commiting dist folder

--- dist/firebaseRefService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var FirebaseRefService = (function () {
-    function FirebaseRefService() {
-    }
-    return FirebaseRefService;
-}());
-exports.FirebaseRefService = FirebaseRefService;
--- dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./baseRoute"));
-__export(require("./indexRoute"));
-__export(require("./voteRoute"));
